9657a561151940355b377d5e28c20234c1af7b92
Marian <amarian12@users.noreply.github.com>
2016-12-09 14:44:32 +0100

    update from GMC to GAME

--- p2pool/bitcoin/networks/gamecredits.py
+++ p2pool/bitcoin/networks/gamecredits.py
@@ -17,7 +17,7 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 90 # s
-SYMBOL = 'GMC'
+SYMBOL = 'GAME'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'gamecredits') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/gamecredits/') if platform.system() == 'Darwin' else os.path.expanduser('~/.gamecredits'), 'gamecredits.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://159.203.226.245:3000/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://159.203.226.245:3000/address/'
